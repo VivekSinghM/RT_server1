c52551c605c59f52493cb9105c745a71843f22eb
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-13 04:39:53 +0530

    heroku pg

--- constant.py
+++ constant.py
@@ -7,8 +7,8 @@ secret_key = "854ea1961c38c22e3765ae19ff9530e2"
 oracle, postgress, mysql= 'oracle','postgress','mysql'
 SQL_URI={
     oracle: 'oracle://hr:hr@localhost:1521/xe',
-    postgress: 'postgresql://postgres:root@localhost/RTM_DB',
-    # postgress: 'postgres://jdhnqbriwmirpq:5939355f63dc41632b9f5ff907a9e6db6f67bf9e6278bbf04df78600f230eb52@ec2-44-208-88-195.compute-1.amazonaws.com:5432/deq22c1ut0lokj',
+    # postgress: 'postgresql://postgres:root@localhost/RTM_DB',
+    postgress: 'postgresql://jdhnqbriwmirpq:5939355f63dc41632b9f5ff907a9e6db6f67bf9e6278bbf04df78600f230eb52@ec2-44-208-88-195.compute-1.amazonaws.com:5432/deq22c1ut0lokj',
     mysql: 'mysql://root:root@localhost/day16'
 }
 mongodb='mogodb'
