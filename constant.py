ed5b5887e3d4b7fcf812d2977408d49efcd88add
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-13 12:49:21 +0530

    pg link

--- constant.py
+++ constant.py
@@ -8,7 +8,7 @@ oracle, postgress, mysql= 'oracle','postgress','mysql'
 SQL_URI={
     oracle: 'oracle://hr:hr@localhost:1521/xe',
     # postgress: 'postgresql://postgres:root@localhost/RTM_DB',
-    postgress: 'postgresql://aatdbfueskfdda:4e4781ba808e52bdf9fca9b1a69a3d7e160a04bd5dc7cecb8c7749a2ac3ec51e@ec2-3-225-110-188.compute-1.amazonaws.com:5432/dcisf9fonmofjo',
+    postgress: 'postgres://aatdbfueskfdda:4e4781ba808e52bdf9fca9b1a69a3d7e160a04bd5dc7cecb8c7749a2ac3ec51e@ec2-3-225-110-188.compute-1.amazonaws.com:5432/dcisf9fonmofjo',
     mysql: 'mysql://root:root@localhost/day16'
 }
 mongodb='mogodb'
