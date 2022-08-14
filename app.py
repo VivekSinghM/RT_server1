519ea35981d3088f777dd9de05bf3036013e4913
VivekSinghM <viveksinghmongare@gamil.com>
2022-08-14 09:22:34 +0530

    add CORS

--- app.py
+++ app.py
@@ -5,11 +5,13 @@
 #     app.run(debug=True)
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from constant import *
 from services.token_manager import Token
 
 app = Flask(__name__)
 app.secret_key = secret_key
+CORS(app)
 
 import api.authentication
 import api.table_manager
